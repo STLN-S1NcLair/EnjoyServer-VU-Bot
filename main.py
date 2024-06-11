e83427a9786c7a178b02ac0a56f910e165b3c531
STLN-S1NcLair <150924423+STLN-S1NcLair@users.noreply.github.com>
2024-06-11 20:50:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import discord
 import os
 from keep_alive import keep_alive
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="?", intents=discord.Intents.all())
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -22,6 +25,35 @@ async def on_message(message):
     if message.content.startswith('$reaction'):
         emoji ="👍"
         await message.add_reaction(emoji)
+
+class SampleView(discord.ui.View): # UIキットを利用するためにdiscord.ui.Viewを継承する
+    def __init__(self, timeout=180): # Viewにはtimeoutがあり、初期値は180(s)である
+        super().__init__(timeout=timeout)
+        
+    @discord.ui.button(label="観戦モード ON", style=discord.ButtonStyle.success)
+    async def observe_on(self, button: discord.ui.Button, interaction: discord.Interaction):
+        const reply;
+        if ' //' in interaction.user.nickname:
+            reply = await interaction.response.send_message(f"{interaction.user.mention} すでに観戦中です！")
+        else:
+            reply = await interaction.response.send_message(f"{interaction.user.mention} 観戦モードにしました！")
+            await interaction.user.setNickname(interaction.user.nickname + " //観戦中")
+        await reply.delete(5000)
+
+    @discord.ui.button(label="観戦モード OFF", style=discord.ButtonStyle.danger)
+    async def observe_off(self, button: discord.ui.Button, interaction: discord.Interaction):
+        const reply;
+        if ' //' in interaction.user.nickname:
+            reply = await interaction.response.send_message(f"{interaction.user.mention} 観戦モードを解除しました！")
+            await interaction.user.setNickname(interaction.user.nickname.split(" //")[0])
+        else:
+            reply = await interaction.response.send_message(f"{interaction.user.mention} すでに観戦モードです！")
+        await reply.delete(5000)
+
+@bot.command()
+async def test(ctx):
+    view = SampleView(timeout=None)
+    await ctx.send(view=view)
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 # Web サーバの立ち上げ
