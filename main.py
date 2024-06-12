6752bce977b93c1edda7fbdfe738656bbfa137dd
STLN-S1NcLair <150924423+STLN-S1NcLair@users.noreply.github.com>
2024-06-12 21:30:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ class SampleView(discord.ui.View): # UIキットを利用するためにdiscord.
 @client.tree.command(name="observer_button", description="観戦モード ON/OFFを追加します")
 async def observer_button(interaction: Interaction):
     view = SampleView(timeout=None)
-    await interaction.response.send_message(view=view)
+    await interaction.response.send_message(content="観戦モード設定", view=view)
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 # Web サーバの立ち上げ
