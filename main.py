68ddff3aec182ab133aa2a079708c4fde6a68909
potyama <kamikaze117117@gmail.com>
2024-06-13 23:25:12 +0900

    Support for cases where interaction.user.nick is set to None.

--- main.py
+++ main.py
@@ -47,11 +47,12 @@ class SampleView(discord.ui.View): # UIキットを利用するためにdiscord.
         
     @discord.ui.button(label="観戦モード ON", style=discord.ButtonStyle.success)
     async def observe_on(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if ' //' in interaction.user.nick:
+        user_nick = interaction.user.nick or interaction.user.display_name
+        if ' //' in user_nick:
             await interaction.response.send_message(f"{interaction.user.mention} すでに観戦中です！", ephemeral=True)
         else:
             await interaction.response.send_message(f"{interaction.user.mention} 観戦モードにしました！", ephemeral=True)
-            await interaction.user.edit(nick=interaction.user.nick + " //観戦中")
+            await interaction.user.edit(nick=user_nick + " //観戦中")
 
     @discord.ui.button(label="観戦モード OFF", style=discord.ButtonStyle.danger)
     async def observe_off(self, interaction: discord.Interaction, button: discord.ui.Button):
